c4b9a58d03bf0ea199391014063edc7d6609135f
Susovan <susovan.sahoo777@gmail.com>
2023-12-01 22:20:45 +0530

    Created createUserCard() function

--- main.js
+++ main.js
@@ -29,6 +29,37 @@ async function getUserDetails(id) {
 getUserDetails('mikr13')
 // getUserDetails(userId)
 
-function createUserCard(userData) {
+function createUserCard(user) {
+    let userCard = document.createElement('div');
+    userCard.setAttribute('class', 'card');
+    userCard.innerHTML = `
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4 image-box">
+                    <div class="img-bg">
+                        <img src="${user.avatar_url}" alt="">
+                    </div>
+                </div>
+                <div class="col-sm-8 content-box">
+                    <div class="card-body">
+                        <h5 class="card-title ">${user.name}</h5>
+                        <p class="card-text ">${user.bio}</p>
 
+                        <div class="row">
+                            <div class="col-4"><p>Followers: ${user.followers}</p></div>
+                            <div class="col-4"><p>Following: ${user.following}</p></div>
+                            <div class="col-4"><p>Repos: ${user.public_repos}</p></div>
+                            <div class="col-6"><p>Tweeter: ${user.twitter_username}</p></div>
+                            <div class="col-6"><p>Location: ${user.location}</p></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+
+    document.getElementById('main').append(userCard);
 }
+
+let userD = getUserDetails('susovan777');
+createUserCard(userD)
